15653fed09fa386becd34f405901149a8608a02a
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-08-01 17:55:05 +0100

    Moced page settings into a separate file

---
+++ settings.js
@@ -0,0 +1,9 @@
+const pageSettings = {
+  title: "Post-Brexit Counter",
+  pageHeading: "Post-Brexit Counter",
+  bannerText: "Britain actually left the EU...",
+  eventTitle: "Brexit's date:",
+  quoteFileLocation: "https://raw.githubusercontent.com/Dragon8oy/brexit-counter/master/quotes.txt",
+  quoteTimer: 10000,
+  eventDateString: new Date("January 31, 2020 23:00:00")
+};
