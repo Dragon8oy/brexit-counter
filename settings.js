8204fbf13c08c88b7538517e755fdd6b9c2de81e
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-08-01 17:59:13 +0100

    Moved assets into assets folder

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const pageSettings = {
   pageHeading: "Post-Brexit Counter",
   bannerText: "Britain actually left the EU...",
   eventTitle: "Brexit's date:",
-  quoteFileLocation: "https://raw.githubusercontent.com/Dragon8oy/brexit-counter/master/quotes.txt",
+  quoteFileLocation: "https://raw.githubusercontent.com/Dragon8oy/brexit-counter/master/assets/quotes.txt",
   quoteTimer: 10000,
   eventDateString: new Date("January 31, 2020 23:00:00")
 };
