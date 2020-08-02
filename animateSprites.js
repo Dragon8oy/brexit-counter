88f471cbd531ce7ff2823b9d5d3f200c1c6b1e4f
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-08-02 20:50:56 +0100

    Handle lowering the sprites separately, and drastically simplified the logic. Fixes 4+ bugs where the code falls out of sync.

--- animateSprites.js
+++ animateSprites.js
@@ -3,68 +3,55 @@ animateSprite('spriteTwo', 'reverse')
 animateSprite('spriteThree', 'reverse')
 getQuoteList(false, '')
 setTimeout(selectSprite, 1000);
+
+spriteOne = {
+  active: 0
+}
+spriteTwo = {
+  active: 0
+}
+spriteThree = {
+  active: 0
+}
+
 function selectSprite() {
   chooseSprite = Math.floor((Math.random() * 3) + 1);
   if (chooseSprite == '1') {
-    if (spriteOneUp == '1') {
-      animateSprite('spriteOne', 'reverse')
-    } else if (spriteOneUp = '0') {
-      if (runOneAgain == '0') {
-        showQuote('spriteOne', quotes)
-        animateSprite('spriteOne', 'normal')
-      }
+    if (spriteOne.active == '0') {
+      animateSprite('spriteOne', 'normal')
+      showQuote('spriteOne', quotes)
     }
-    runOneAgain = 0
   } else if (chooseSprite == '2') {
-    if (spriteTwoUp == '1') {
-      animateSprite('spriteTwo', 'reverse')
-    } else if (spriteTwoUp = '0') {
-      if (runTwoAgain == '0') {
-        showQuote('spriteTwo', quotes)
-        animateSprite('spriteTwo', 'normal')
-      }
+    if (spriteTwo.active == '0') {
+      animateSprite('spriteTwo', 'normal')
+      showQuote('spriteTwo', quotes)
     }
-    runTwoAgain = 0
   } else if (chooseSprite == '3') {
-    if (spriteThreeUp == '1') {
-      animateSprite('spriteThree', 'reverse')
-    } else if (spriteThreeUp = '0') {
-      if (runThreeAgain == '0') {
-        showQuote('spriteThree', quotes)
-        animateSprite('spriteThree', 'normal')
-      }
+    if (spriteThree.active == '0') {
+      animateSprite('spriteThree', 'normal')
+      showQuote('spriteThree', quotes)
     }
-    runThreeAgain = 0
   }
-  setTimeout(selectSprite, 3500);
+  setTimeout(selectSprite, pageSettings.quoteTimer / 1.5);
 }
 
 function animateSprite(targetSprite, direction) {
     document.getElementById(targetSprite).animate([
     { transform: 'translateY(500px)' }, //Start (Pixels off screen)
     { transform: 'translateY(50px)' } //End
-  ], { 
+  ], {
     duration: 2000,
     fill: 'forwards',
     direction: direction,
     iterations: 1
   });
   if (direction == 'normal') {
-    if (targetSprite == 'spriteOne') {spriteOneUp = '1';};
-    if (targetSprite == 'spriteTwo') {spriteTwoUp = '1';};
-    if (targetSprite == 'spriteThree') {spriteThreeUp = '1';};
+    if (targetSprite == 'spriteOne') {spriteOne.active = '1'; setTimeout( function () {animateSprite('spriteOne', 'reverse')}, pageSettings.quoteTimer)};
+    if (targetSprite == 'spriteTwo') {spriteTwo.active = '1'; setTimeout( function () {animateSprite('spriteTwo', 'reverse')}, pageSettings.quoteTimer)};
+    if (targetSprite == 'spriteThree') {spriteThree.active = '1'; setTimeout( function () {animateSprite('spriteThree', 'reverse')}, pageSettings.quoteTimer)};
   } else {
-    if (targetSprite == 'spriteOne') {
-      spriteOneUp = '0';
-      runOneAgain = '1';
-    };
-    if (targetSprite == 'spriteTwo') {
-      spriteTwoUp = '0';
-      runTwoAgain = '1';
-    };
-    if (targetSprite == 'spriteThree') {
-      spriteThreeUp = '0';
-      runThreeAgain = '1';
-    };
+    if (targetSprite == 'spriteOne') {spriteOne.active = '0'};
+    if (targetSprite == 'spriteTwo') {spriteTwo.active = '0'};
+    if (targetSprite == 'spriteThree') {spriteThree.active = '0'};
   }
 }
