f963556ed9d93f244a67b66d2fe0fe95b228cdf3
Dragon8oy <stuart.a.hayhurst@gmail.com>
2021-03-27 14:51:31 +0000

    Added years ago line, if yearsAgo is above 1

--- js/dateCalc.js
+++ js/dateCalc.js
@@ -2,9 +2,10 @@
 today = new Date();
 document.getElementById("eventDisplay").innerHTML = pageSettings.eventDateString.getDate() + '/' + pageSettings.eventDateString.getMonth() + 1 + '/' + pageSettings.eventDateString.getFullYear();
 
-const oneHour = 1000*60*60;
-const oneDay = 1000*60*60*24;
-const oneWeek = 1000*60*60*24*7;
+const oneHour = 1000 * 60 * 60;
+const oneDay = oneHour * 24;
+const oneWeek = oneDay * 7;
+//const oneYear = oneWeek * 52
 
 //Timer to refresh dates every 1 second
 update()
@@ -20,17 +21,33 @@ function update() {
 //Work out how long since event passed
 function calculateTime() {
   today = new Date();
-  timeDiff = today.getTime()-pageSettings.eventDateString.getTime();
+  timeDiff = today.getTime() - pageSettings.eventDateString.getTime();
+
+  //Work out years ago
   hoursAgo = Math.floor(timeDiff / oneHour);
   daysAgo = Math.floor(hoursAgo / 24);
   weeksAgo = Math.floor(daysAgo / 7);
-  daysAgo = Math.floor(daysAgo - (weeksAgo * 7));
-  hoursAgo = Math.floor((timeDiff / oneHour) - ((weeksAgo * 168) + (daysAgo * 24)));
+  yearsAgo = Math.floor(weeksAgo / 52)
+
+  //Refine weeks ago
+  weeksAgo = Math.floor(weeksAgo - (yearsAgo * 52))
+
+  //Refine days ago
+  daysAgo = Math.floor(daysAgo - (((yearsAgo * 52) + weeksAgo) * 7));
+
+  //Refine hours ago
+  hoursAgo = Math.floor((timeDiff / oneHour) - ((((yearsAgo * 52) + weeksAgo) * 7) + daysAgo) * 24);
 }
 
 //Display the calculated time diffs
 function displayTime() {
+  //If yearsAgo is 0, hide the element
+  if (yearsAgo == 0) {
+    document.getElementById("yearsAgoHtmlTitle").style.display = "none"
+    document.getElementById("yearsAgoHtml").style.display = "none"
+  }
   document.getElementById("hoursAgoHtml").innerHTML = hoursAgo;
   document.getElementById("daysAgoHtml").innerHTML = daysAgo;
   document.getElementById("weeksAgoHtml").innerHTML = weeksAgo;
+  document.getElementById("yearsAgoHtml").innerHTML = yearsAgo;
 }
